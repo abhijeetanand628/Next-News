850ef7f69c958161807df03d10f1d940146b96e9
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-11-22 00:16:41 +0530

    Made the header responsive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,13 +3,20 @@ import { Search } from 'lucide-react';
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-between mt-4'>
-        <h1 className='ml-12 cursor-pointer text-gray-700 hover:text-black'>NextNews</h1>
+    <div className='flex items-center justify-between px-4 sm:px-6 md:px-12 lg:px-24 mt-4'>
+        <h1 className='cursor-pointer text-gray-700 hover:text-black text-lg sm:text-xl md:text-xl'>
+          NextNews
+        </h1>
 
-        <div className='flex items-center justify-end gap-8 mr-24'>
-            <Search className='text-gray-700 hover:text-black cursor-pointer' size={18} /> 
+        <div className='flex items-center justify-end gap-6 sm:gap-6 md:gap-8'>
+            <Search 
+              className='text-gray-700 hover:text-black cursor-pointer' 
+              size={18} 
+            /> 
 
-            <div className='text-gray-700 text-lg hover:text-black cursor-pointer'>☰</div>      
+            <div className='text-gray-700 text-lg hover:text-black cursor-pointer'>
+              ☰
+            </div>      
         </div>
     </div>
   )
