7efb7b9598d7015b06e9a60759d1d18f84ca3bac
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-11-22 16:49:46 +0530

    Added footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-200/30 flex items-center justify-center'>
+        <h1 className='text-[#949494] font-bold py-4'>Copyright © 2026 Next News</h1>
+    </div>
+  )
+}
+
+export default Footer
