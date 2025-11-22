952a5e96697edc63a27ff751b15e975a9558f7f9
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-11-22 16:43:36 +0530

    Made the header text functional

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,20 @@
+'use client'
 import { Search } from 'lucide-react';
 
 const Header = () => {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
   return (
-    <div className='flex items-center justify-between px-4 sm:px-6 md:px-12 lg:px-20 mt-4'>
-        <h1 className='cursor-pointer text-gray-700 hover:text-black text-lg sm:text-xl md:text-xl'>
+    <div className='flex items-center justify-between px-4 sm:px-6 md:px-12 lg:px-20 py-4 sticky top-0 z-50 bg-white/70 backdrop-blur-md'>
+        <h1 
+          onClick={scrollToTop}
+          className='cursor-pointer text-gray-700 hover:text-black text-lg sm:text-xl md:text-xl'>
           NextNews
         </h1>
 
